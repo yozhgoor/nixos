8a3f3a8240b64533918e354dcc52de7959afb850
yozhgoor <yozhgoor@outlook.com>
2025-03-09 16:12:10 +0100

    Update workflow file

--- flake.nix
+++ flake.nix
@@ -9,24 +9,17 @@
     };
   };
 
-  outputs = { self, nixpkgs, ...} @ inputs:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in
-  {
-    nixosConfigurations = {
-      sanctuary = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./configuration.nix
-          inputs.home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-          }
-	      ];
-      };
+  outputs = { self, nixpkgs, ...} @ inputs: {
+    nixosConfigurations.sanctuary = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        inputs.home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+        }
+      ];
     };
   };
 }
