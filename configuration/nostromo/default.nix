56802e13f95e5881b50fed8aee2157424b9f18b1
yozhgoor <yozhgoor@outlook.com>
2025-04-17 14:08:43 +0200

    Typo

--- configuration/nostromo/default.nix
+++ configuration/nostromo/default.nix
@@ -35,7 +35,7 @@
     after = [ "dev-ttyAMA0.device" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.bluez}/bin/btattach -B /dev/ttyAMA0 -p bcm -S 3000000";
+      ExecStart = "${pkgs.bluez}/bin/btattach -B /dev/ttyAMA0 -P bcm -S 3000000";
     };
   };
 
